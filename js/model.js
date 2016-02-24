39eb20c59020c661eaf69c8b8e9f0cb7a2731d15
Shamwow16 <shamyleg@gmail.com>
2016-02-24 11:33:32 -0600

    Create geoLocations array and set neighborhood coords

--- js/model.js
+++ js/model.js
@@ -1,10 +1,39 @@
+var neighborhood = {
+	name : "Ravenswood, Chicago",
+	latitude : 41.968667,
+	longitude : -87.674609
+}
+
+var geoLocations = [
+	{
+		name:'Big Jones',
+		latitude: 41.979444,
+		longitude:-87.668036,
+		streetAddress: "5347 N Clark St"},
+	{
+		name:'Old Town School of Folk Music',
+		latitude: 41.964223 ,
+		longitude: -87.686013 ,
+		streetAddress: "4544 North Lincoln Avenue"
+					},
+	{
+		name:'Riviera Theatre',
+		latitude: 41.968852 ,
+		longitude: -87.659878,
+		streetAddress: "4746 N Racine Ave"
+
+					}
+]
 
 var geoLocation = function(){
-	this.personName = ko.observable('Bob');
-	this.personAge = ko.observable(123);
-	this.streetAddress = ko.observable('');
-	this.city = ko.observable('');
-	this.submitSuccess = ko.observable('Submission');
+	/*this.personName = ko.observable('Bob');
+	this.personAge = ko.observable(123);*/
+	var self=this;
+	self.name = ko.observable('');
+	self.longitude = ko.observable('');
+	self.latitude = ko.observable('');
+	self.streetAddress = ko.observable('');
+	self.city = "Chicago, IL";
 };
 
 var ViewModel = function(){

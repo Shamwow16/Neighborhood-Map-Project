ff6cfca556c0aeb4043e30b3c7ae3e0ae6ab01bd
Shamwow16 <shamyleg@gmail.com>
2016-02-24 10:24:44 -0600

    Create initMap function, add geoLocations array

--- js/map-view.js
+++ js/map-view.js
@@ -1,2 +1,10 @@
-function initMap(){};
-/*var map = new google.maps.Map();*/
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+
+var geoLocations = ["Big Jones", "Roots Handmade Pizza", "Mariano's", "CVS", "L.A Fitness"];
+

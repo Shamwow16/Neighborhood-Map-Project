12153169dca099bc7f3bb2d47d0b4299933bfdd9
Shamwow16 <shamyleg@gmail.com>
2016-02-24 11:33:50 -0600

    create for loop for markers

--- js/map-view.js
+++ js/map-view.js
@@ -1,10 +1,22 @@
+
+
 var map;
       function initMap() {
+
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+
+         center:({lat:neighborhood.latitude, lng:neighborhood.longitude}),
+          zoom: 14
         });
+
+        for(var i=0; i<geoLocations.length ; i++){
+        var marker = new google.maps.Marker({
+    	position: {lat:geoLocations[i].latitude, lng:geoLocations[i].longitude},
+    	map: map,
+    	title: geoLocations[i].name
+    })
+  	};
+
       }
 
-var geoLocations = ["Big Jones", "Roots Handmade Pizza", "Mariano's", "CVS", "L.A Fitness"];
 

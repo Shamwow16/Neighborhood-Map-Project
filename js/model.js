15590084b1bc640efb22691dec14ed8bc3ccfaf0
Shamwow16 <shamyleg@gmail.com>
2016-02-23 22:08:53 -0600

    Add street address and city. Apply binding to ko.

--- js/model.js
+++ js/model.js
@@ -1,7 +1,11 @@
 var myViewModel = {
-	personName: 'Bob',
-	personAge: 123
+	personName: ko.observable('Bob'),
+	personAge: ko.observable(123),
+	streetAddress: ko.observable(''),
+	city: ko.observable('')
 };
 
 
+
 ko.applyBindings(myViewModel);
+
